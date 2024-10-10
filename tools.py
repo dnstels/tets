1898c505e1c4ae5ec76b49ba9acdd98d68a3d77f
dnstels <35730176+dnstels@users.noreply.github.com>
2024-10-10 17:56:00 +0400

    Update tools.py

--- tools.py
+++ tools.py
@@ -5,7 +5,7 @@ class JsonClassEncoder(json.JSONEncoder):
     def default(self, obj):
         # if isinstance(obj, Address):
         # return obj.__dict__
-        out={}
-        for i_attr in [attr for attr in dir(obj) if not callable(getattr(obj, attr)) and not attr.startswith("_")]:
-            out[i_attr]=obj.__getattribute__(i_attr)
-        return out
+        # out={}
+        # for i_attr in [attr for attr in dir(obj) if not callable(getattr(obj, attr)) and not attr.startswith("_")]:
+        #     out[i_attr]=obj.__getattribute__(i_attr)
+        return {i_attr:obj.__getattribute__(i_attr) for i_attr in [attr for attr in dir(obj) if not callable(getattr(obj, attr)) and not attr.startswith("_")]}
